8b7edc0ee1f14ab9cd3a84c4ebebbb746d457d4f
mark-williams <markwill@hotmail.co.uk>
2016-12-07 16:47:22 +0000

    Initial

---
+++ src/__tests__/Calculator-spec.js
@@ -0,0 +1,23 @@
+import * as calculator from '../Calculator';
+
+describe('Calculator', () => {
+  it('should add two numbers', () => {
+    const sum = calculator.add(5, 2);
+    expect(sum).toBe(7);
+  });
+
+  it('should subtract two numbers', () => {
+    const sub = calculator.subtract(5, 2);
+    expect(sub).toBe(3);
+  });
+
+  it('should multiply two numbers', () => {
+    const sub = calculator.multiply(5, 2);
+    expect(sub).toBe(10);
+  });
+
+  it('should divide two numbers', () => {
+    const sub = calculator.divide(100, 50);
+    expect(sub).toBe(2);
+  });
+});
